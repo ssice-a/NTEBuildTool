5442fa3fb7b3d3fa11c8d0ca2a91d1d210d4b3c6
ssice-a <2548777674@qq.com>
2026-08-01 16:11:13 +0800

    Remove tracked scratch build artifacts

--- .scratch/PluginBuild_GameMaterialLibrary_Strict/Source/HTGame/Public/HTGame.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2026 NTEBuildTool contributors.
-
-#pragma once
-
-#include "Modules/ModuleManager.h"
-
-class FHTGameModule final : public IModuleInterface
-{
-};
--- .scratch/PluginBuild_PhysicsPresetLibrary_Strict2/Source/HTGame/Private/HTGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2026 NTEBuildTool contributors.
-
-#include "HTGame.h"
-
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FHTGameModule, HTGame)
